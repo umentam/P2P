625c40cbf6dbb4d306ea4858ae081946837a1f35
Michael Umenta <umentam@gmail.com>
2015-07-12 18:43:57 -0700

    set up project for AngularJS, modified the signup and the home page as well as cleaned up some code

---
+++ js/app.js
@@ -0,0 +1,52 @@
+'use strict';
+var app = angular.module("P2pApp", [])
+    .controller('IndexController', ['$scope', '$location', function ($scope, $location) {
+        $scope.userInfo = [
+            {
+                name: 'name',
+                properName: 'Name',
+                input: ''
+            },
+            {
+                name: 'email',
+                properName: 'Email',
+                isUnique: false, 
+                input: ''
+            },
+            {
+                name: 'password',
+                properName: 'Password',
+                isConfirmed: false, 
+                input: ''
+            },
+            {
+                name: 'location',
+                properName: 'Location',
+                isConfirmed: false, 
+                input: ''
+            }];
+
+        $scope.homeButtons = {
+            login: "Login",
+            signup: "Sign Up"
+        };
+        $scope.changeUrl = function (path) {
+            $location.path(path);
+        };
+    }]);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/home.html'
+        })
+        .when('/signup', {
+            templateUrl: 'views/signup.html'
+        })
+        .when('/login', {
+            templateUrl: 'views/login.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
